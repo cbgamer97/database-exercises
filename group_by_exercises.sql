44985778ad52bd6a615cd2e2f315bc4b60d2bdac
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-05-03 16:13:05 -0500

    completed walkthrough on exercise

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,13 +2,13 @@ USE employees;
 
 SELECT DISTINCT title FROM titles;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE 'E%'
+SELECT last_name FROM employees WHERE last_name LIKE 'E%'
                           AND last_name LIKE '%e' GROUP BY last_name;
 
-SELECT first_name, COUNT(last_name) FROM employees WHERE last_name LIKE 'E%'
-                                           AND last_name LIKE '%e' GROUP BY first_name;
+SELECT first_name, last_name FROM employees WHERE last_name LIKE 'E%'
+                                  AND last_name LIKE '%e' GROUP BY first_name, last_name;
 
-SELECT DISTINCT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%';
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%' GROUP BY last_name;
 
 SELECT DISTINCT last_name, COUNT(first_name) FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'
                                                                             GROUP BY last_name;
