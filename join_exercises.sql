c864ca8ab3c2bf4a3b9f494a7cfbc1aea21e298e
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-05-05 10:01:13 -0500

    completed bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -32,3 +32,15 @@ JOIN salaries s on e.emp_no = s.emp_no
 WHERE s.to_date LIKE '9%'
 AND dm.to_date LIKE '9%'
 ORDER BY dept_name;
+
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee,
+    dept_name AS Department,
+    CONCAT(e2.first_name, ' ', e2.last_name) AS Manager
+FROM employees e
+JOIN dept_emp de on e.emp_no = de.emp_no
+JOIN departments d on d.dept_no = de.dept_no
+JOIN dept_manager dm on d.dept_no = dm.dept_no
+JOIN employees e2 on e2.emp_no = dm.emp_no
+WHERE de.to_date LIKE '9%'
+AND dm.to_date LIKE '9%'
+ORDER BY dept_name, e.emp_no;
