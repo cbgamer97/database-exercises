95b74046a87ef54c37057ed5a1e3f4738294bc63
Connor Brinkley <connor.f.brinkley@gmail.com>
2023-05-15 12:18:25 -0500

    pushing exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,11 @@
+USE employees;
+
+SELECT * FROM employees WHERE hire_date
+IN (SELECT hire_date FROM employees WHERE emp_no = 101010);
+
+SELECT * FROM titles WHERE emp_no IN
+(SELECT emp_no FROM employees WHERE first_name = 'Aamod');
+
+SELECT first_name, last_name FROM employees
+WHERE emp_no IN (SELECT emp_no FROM dept_manager WHERE gender = 'F');
+
